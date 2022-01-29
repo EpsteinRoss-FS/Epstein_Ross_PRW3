5fa98010ca2891cf3b3928a0e9f9f585d7af99fd
EpsteinRoss-FS <74941767+EpsteinRoss-FS@users.noreply.github.com>
2022-01-29 12:10:32 -0500

    fetches, but use state not properly setting

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,45 +1,79 @@
 /* This example requires Tailwind CSS v2.0+ */
 import { PaperClipIcon } from '@heroicons/react/solid'
-import React from "react";
+import React, {useEffect, useState} from "react";
 
-class Profile extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-        profile: {userName: "Psherman32", firstName: 'Peter', lastName:"Sherman", address:"32 Wallaby Way", city: "Sydney",
-            state: "AU", postCode:"40019", password: "psherman32wallabywaysydney", phone: "502-555-5555",},
-        };
+export default function Profile() {
+    const [error, setError] = useState(null);
+    const [profile, setProfile] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+
+    useEffect(async () => {
+        async function fetchData() {
+            await fetch("https://randomuser.me/api/")
+                .then(res => res.json())
+                .then(
+                    async (result) => {
+
+                        let profileData = result.results[0];
+                        // await setProfile(result.results);
+                        await setProfile(result);
+                        setIsLoaded(true);
+                        console.log(profile);
+
+                    },
+
+                    (error) => {
+                        setIsLoaded(true);
+                        setError(error);
+                    }
+                ).then(
+
+                )
+        }
+            await fetchData()
+
+    } ,
+[])
+
+
+    if (error) {
+        return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+        return <div>Loading...</div>;
+    } else {
+        return (
+
+            <div className="mt-2 mb-5 m-auto w-50 bg-light shadow-lg border-2 border overflow-hidden sm:rounded-lg">
+                <div className="px-4 py-5 sm:px-6">
+                    <h3 className="text-lg leading-6 font-medium text-gray-900"> Profile </h3>
+                    {/*<p className="mt-1 max-w-2xl text-sm text-gray-500">Personal details and application.</p>*/}
+                </div>
+                <div className="border-t border-gray-200">
+                    <dl>
+                        <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                            <dt className="text-sm font-medium text-gray-500">Full name</dt>
+                            <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">Margot Foster</dd>
+                        </div>
+                        <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                            <dt className="text-sm font-medium text-gray-500">Address</dt>
+                            <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">Backend Developer</dd>
+                        </div>
+                        <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                            <dt className="text-sm font-medium text-gray-500">Email address</dt>
+                            <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">margotfoster@example.com</dd>
+                        </div>
+                        <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                            <dt className="text-sm font-medium text-gray-500">Phone Number</dt>
+                            <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">502-555-5555</dd>
+                        </div>
+                        <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                            <dt className="text-sm font-medium text-gray-500">Password</dt>
+                            <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">PW FIELD</dd>
+                        </div>
+                    </dl>
+                </div>
+            </div>
+        )
     }
-    render(){
-    return (
-
-        <div className="mt-2 mb-5 m-auto w-50 bg-light shadow-lg border-2 border overflow-hidden sm:rounded-lg">
-            <div className="px-4 py-5 sm:px-6">
-                <h3 className="text-lg leading-6 font-medium text-gray-900">Username's Profile </h3>
-                {/*<p className="mt-1 max-w-2xl text-sm text-gray-500">Personal details and application.</p>*/}
-            </div>
-            <div className="border-t border-gray-200">
-                <dl>
-                    <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                        <dt className="text-sm font-medium text-gray-500">Full name</dt>
-                        <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">{this.state.profile.firstName} {this.state.profile.lastName}</dd>
-                    </div>
-                    <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                        <dt className="text-sm font-medium text-gray-500">Address</dt>
-                        <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">Backend Developer</dd>
-                    </div>
-                    <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                        <dt className="text-sm font-medium text-gray-500">Phone Number</dt>
-                        <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">502-555-5555</dd>
-                    </div>
-                    <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                        <dt className="text-sm font-medium text-gray-500">Password</dt>
-                        <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">PW FIELD</dd>
-                    </div>
-                </dl>
-            </div>
-        </div>
-    )}
 }
 
-export default Profile;
