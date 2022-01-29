87e3babba67745f09fa61344ca35272feef6d790
EpsteinRoss-FS <74941767+EpsteinRoss-FS@users.noreply.github.com>
2022-01-29 13:37:24 -0500

    name and address fields fill

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -7,6 +7,10 @@ export default function Profile() {
     const [profile, setProfile] = useState( {
         firstName: '',
         lastName: '',
+        streetName: '',
+        city: '',
+        state: '',
+        zip: '',
 
     });
     const [isLoaded, setIsLoaded] = useState(false);
@@ -26,6 +30,10 @@ export default function Profile() {
                             ...prevState,
                                 firstName: profileData.name.first,
                                 lastName: profileData.name.last,
+                                streetName: profileData.location.street.number + " " + profileData.location.street.name,
+                                city: profileData.location.city,
+                                state: profileData.location.state,
+                                zip: profileData.location.postcode
                             }
                             )
                         );
@@ -65,7 +73,10 @@ export default function Profile() {
                         </div>
                         <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                             <dt className="text-sm font-medium text-gray-500">Address</dt>
-                            <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">Backend Developer</dd>
+                            <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                                {profile.streetName}
+                                <p>{profile.city}, {profile.state} {profile.zip}</p>
+                            </dd>
                         </div>
                         <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                             <dt className="text-sm font-medium text-gray-500">Email address</dt>
